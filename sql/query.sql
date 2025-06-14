0dfd725d536a071eb951320427dd275f1427c512
Christian <christian0511@gmx.de>
2025-06-14 21:24:44 +0200

    formatting

--- sql/query.sql
+++ sql/query.sql
@@ -1,18 +1,28 @@
 -- name: GetAuthor :one
-SELECT * FROM authors
-WHERE id = ? LIMIT 1;
+SELECT
+    *
+FROM
+    authors
+WHERE
+    id = ?
+LIMIT
+    1;
 
 -- name: ListAuthors :many
-SELECT * FROM authors
-ORDER BY name;
+SELECT
+    *
+FROM
+    authors
+ORDER BY
+    name;
 
 -- name: CreateAuthor :execresult
-INSERT INTO authors (
-  name, bio
-) VALUES (
-  ?, ?
-);
+INSERT INTO
+    authors (name, bio)
+VALUES
+    (?, ?);
 
 -- name: DeleteAuthor :exec
 DELETE FROM authors
-WHERE id = ?;
+WHERE
+    id = ?;
